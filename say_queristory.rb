ce22261a4739e7c8e1b619a1bce979b6deb09a44
daichi <daipanchi@gmail.com>
2015-01-11 15:43:36 +0900

    Make Alex to speech when the query is english

--- say_queristory.rb
+++ say_queristory.rb
@@ -3,6 +3,13 @@ require 'tweetstream'
 require 'dotenv'
 require 'uri'
 
+def has_multi_byte?(str)
+  str.bytes do |b|
+    return true if  (b & 0b10000000) != 0
+  end
+  false
+end
+
 TweetStream.configure do |config|
   Dotenv.load
   config.consumer_key       = ENV['CONSUMER_KEY']
@@ -13,5 +20,13 @@ TweetStream.configure do |config|
 end
 
 TweetStream::Client.new.follow(2503690447) do |status|
-  `say "#{status.text.gsub(URI.regexp(['http', 'https']), '')}"`
+  text_to_speech = status.text.gsub(URI.regexp(['http', 'https']), '')
+
+  if has_multi_byte?(text_to_speech)
+    text_to_speech = text_to_speech.gsub(' ', '、')
+  else
+    options = '-vAlex'
+  end
+
+  `say "#{options}" "#{text_to_speech}"`
 end
